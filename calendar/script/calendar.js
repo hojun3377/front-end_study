64e1557fb8203ed8124aead52a886b0918e7acac
hojun3377 <hojun3377@gmail.com>
2021-09-16 07:46:33 +0900

    Calendar UI_11

--- calendar/script/calendar.js
+++ calendar/script/calendar.js
@@ -1,22 +1,28 @@
 'use strict'
-// db json
 const dbJSON = {
-  // d_2021_9: '{"2":{"0":["sdf","dsf"],"2":["asd","ddd"]}}'
+  d_2021_9 : '{"13":{"430":["기상","커피마시기"],"450":["에이콘아카데미"],"1080":["퇴근"],"1170":["집 도착"],"1328":["취침"]},"15":{"360":["자차","카","타파하"]}}'
+  // d_2021_9 : {
+  //   13: {
+  //     430: ["기상","커피마시기"],
+  //     450: ["에이콘아카데미"],
+  //     1080: ["퇴근"],
+  //     1170: ["집 도착"],
+  //     1328: ["취침"]
+  //   },
+  //   15: {
+  //     360: ["자차","카","타파하"]
+  //   }
+  // }
 }
-
-/////////////////////////////////////////////////////////////////////////
-
 // Selector
-
-// calendar
 const calendarControler = document.getElementById("calendarControler");
 const calendarTitle = calendarControler.getElementsByClassName("title")[0];
 const calendarTable = document.getElementById("calendarTable");
 const calendarDateEl = calendarTable.getElementsByTagName("td");
 const calendarSchedule = calendarTable.getElementsByClassName("schedule");
-const crtMonthIpt = calendarControler.getElementsByClassName("btn-container")[0].children[1];
+const crtMonthIpt = calendarControler.getElementsByClassName("btn-container")[0].children[2];
 const preMonthBtn = calendarControler.getElementsByClassName("btn-container")[0].children[0];
-const nextMonthBtn = calendarControler.getElementsByClassName("btn-container")[0].children[2];
+const nextMonthBtn = calendarControler.getElementsByClassName("btn-container")[0].children[1];
 // modal
 const calendarModal = document.getElementById("calendarModal");
 const modalDate = calendarModal.getElementsByClassName("date")[0];
@@ -28,33 +34,34 @@ const modalSchedule = modalForm.getElementsByClassName("modal-schedule")[0];
 // js
 
 // calendar constructor & function
-const Calendar = function(year=new Date().getFullYear(),month=new Date().getMonth()+1) {
+const Calendar = function (year=0, month=1, scheduler=new Scheduler(year, month)) {
   this.year = year;
   this.month = month;
-  this.firstDay = new Date(year,month-1,1).getDay();
-  this.lastDate = new Date(year,month,0).getDate();
-  this.preMonthStartDate = new Date(year,month,1-this.firstDay).getDate();
+  this.firstDay = new Date(year, month-1, 1).getDay();
+  this.lastDate = new Date(year, month, 0).getDate();
+  this.preMonthStartDate = new Date(year, month, 1-this.firstDay).getDate();
   this.title = `${year}년 ${month}월`;
   // this.value = year+"-"+(month<10 ? "0"+month : month);
-  this.value = year+"-"+(month+"").padStart(2,0);
+  this.value = year + "-" + (month + "").padStart(2, 0);
   this.preValue = `${month==1 ? year-1 : year}-${month==1 ? 12 : month-1}`;
-  this.nextValue = `${year+(month==12 ? 1 : 0)}-${month==12 ? 1 : month+1}`;
+  this.nextValue = `${year + (month==12 ? 1 : 0)}-${month==12 ? 1 : month+1}`;
   this.schedule = undefined;
-
-  this.setCalendar = (year,month) => {
+  this.scheduler = scheduler;
+  
+  this.setCalendar = (year, month) => {
     year = Number(year);
     month = Number(month);
-
+    
     this.year = year;
     this.month = month;
-    this.firstDay = new Date(year,month-1,1).getDay();
-    this.lastDate = new Date(year,month,0).getDate();
-    this.preMonthStartDate = new Date(year,month,1-this.firstDay).getDate();
+    this.firstDay = new Date(year, month-1, 1).getDay();
+    this.lastDate = new Date(year, month, 0).getDate();
+    this.preMonthStartDate = new Date(year, month, 1-this.firstDay).getDate();
     this.title = `${year}년 ${month}월`;
-    this.value = year+"-"+(month+"").padStart(2,0);
+    this.value = year+"-" +(month+"").padStart(2, 0);
     this.preValue = `${month==1 ? year-1 : year}-${month==1 ? 12 : month-1}`;
     this.nextValue = `${year+(month==12 ? 1 : 0)}-${month==12 ? 1 : month+1}`;
-  }
+  };
   this.setSchedule = schedule => {
     try {
       this.schedule = JSON.parse(schedule);
@@ -62,19 +69,55 @@ const Calendar = function(year=new Date().getFullYear(),month=new Date().getMont
     catch(e) {
       console.log(e);
       this.schedule = {};
-      dbJSON[`d_${this.year}_${this.month}`] = JSON.stringify(this.schedule);
+      // dbJSON[`d_${this.year}_${this.month}`] = JSON.stringify(this.schedule);
+    }
+  };
+  this.setScheduler = scheduler => {
+    this.scheduler = scheduler;
+  };
+}
+const Scheduler = function(year=0, month=1, date=1) {
+  this.year = year;
+  this.month = month;
+  this.date = date;
+  this.title = `${this.year}년 ${this.month}월 ${this.date}일`;
+  this.schedule = undefined;
+  this.target = undefined;
+
+  this.setScheduler = (year=0, month=1, date=1) => {
+    this.year = year;
+    this.month = month;
+    this.date = date;
+    this.title = `${this.year}년 ${this.month}월 ${this.date}일`;
+    this.schedule = undefined;
+    this.target = undefined;
+  };
+  this.setDate = date => {
+    this.date = date;
+    this.title = `${this.year}년 ${this.month}월 ${this.date}일`;
+  }
+  this.setSchedule = schedule => {
+    try {
+      this.schedule = JSON.parse(schedule);
+    }
+    catch (e) {
+      console.log(e);
+      this.schedule = {};
     }
   }
-}
-const calendarPrint = function(calendar) {
+  this.setTarget = target => {
+    this.target = target;
+  };
+};
+const calendarPrint = function (calendar) {
   calendarTitle.innerText = calendar.title;
-
+  
   crtMonthIpt.value = calendar.value;
   preMonthBtn.value = calendar.preValue;
   nextMonthBtn.value = calendar.nextValue;
-
+  
   let activeFlag,
-      firstIdx = calendar.firstDay,
+    firstIdx = calendar.firstDay,
       lastIdx = calendar.firstDay+calendar.lastDate-1;
 
   Array.from(calendarDateEl).forEach((item, i) => {
@@ -90,10 +133,10 @@ const calendarPrint = function(calendar) {
 
     if(activeFlag) {
       date.innerText = i-calendar.firstDay+1;
-      item.value = { year:calendar.year, month:calendar.month, date:date.innerText };
+      item.value = i-calendar.firstDay+1;
 
-      if(calendar.schedule[date.innerText]!==undefined) {
-        for(let key in calendar.schedule[date.innerText]) {
+      if (calendar.schedule[date.innerText] !== undefined) {
+        for (let key in calendar.schedule[date.innerText]) {
           let scheduleList = document.createElement("li");
           scheduleList.innerText = calendar.schedule[date.innerText][key];
           calendarSchedule[i].append(scheduleList);
@@ -105,8 +148,8 @@ const calendarPrint = function(calendar) {
       item.addEventListener("click", calendarClickEventHandler.date);
     }
     else {
-      if(i<firstIdx) date.innerText = i+calendar.preMonthStartDate;
-      else date.innerText = i-lastIdx;
+      if (i < firstIdx) date.innerText = i + calendar.preMonthStartDate;
+      else date.innerText = i - lastIdx;
       item.value = undefined;
 
       item.classList.remove("active");
@@ -116,58 +159,60 @@ const calendarPrint = function(calendar) {
   });
 };
 const calendarClickEventHandler = {
-  date : e => {
-    let year = e.currentTarget.value.year,
-        month = e.currentTarget.value.month,
-        date = e.currentTarget.value.date;
-
-    modalDate.innerText = `${year}년 ${month}월 ${date}일`;
-    modalDate.value = date;
+  date: e => {
+    calendar.scheduler.setTarget(e.currentTarget);
+    calendar.scheduler.setDate(e.currentTarget.value);
+    
+    let date = calendar.scheduler.date;
+    calendar.scheduler.setSchedule(JSON.stringify(calendar.schedule[date]));
+    
     modalForm.addTime.value = "00:00";
     modalForm.addSchedule.value = "";
     modalForm.addSchedule.placeholder = "추가 스케줄 입력";
-    calendarModal.value = e.currentTarget;
+    modalDate.innerText = calendar.scheduler.title;
 
-    if(calendar.schedule[date]!==undefined) {
-      for(let key in calendar.schedule[date]) {
-        let scheduleList = document.createElement("li"),
-            date = document.createElement("b"),
-            schedule = document.createElement("span"),
-            deleteBtn = document.createElement("button");
-
-        date.classList.add("time");
-        date.innerText = (Number.parseInt(key/60)+"").padStart(2,0)+":"+(key%60+"").padStart(2,0);
-
-        schedule.innerText = calendar.schedule[date][key];
-
-        deleteBtn.type = "button";
-        deleteBtn.value = key;
-        deleteBtn.classList.add("delete");
-        deleteBtn.innerText="x";
-        deleteBtn.onclick = calendarClickEventHandler.modalDltSchedule;
-
-        scheduleList.append(date);
-        scheduleList.append(schedule);
-        scheduleList.append(deleteBtn);
-
-        // schedule.name = "scheduleList";
-        modalSchedule.append(scheduleList);
-        modalSchedule.value = calendar.schedule[date];
+    let dateSchedule = calendar.scheduler.schedule;
+    if (dateSchedule !== undefined) {
+      for(let time in dateSchedule) {
+        dateSchedule[time].forEach((todo,i) => {
+          let scheduleList = document.createElement("li"),
+              date = document.createElement("b"),
+              schedule = document.createElement("span"),
+              deleteBtn = document.createElement("button");
+  
+          date.classList.add("time");
+          date.innerText = (Number.parseInt(time/60)+"").padStart(2,0)+":"+ (time%60+"").padStart(2,0);
+  
+          schedule.innerText = todo;
+  
+          deleteBtn.type = "button";
+          deleteBtn.value = `{"time":${time},"index":${i}}`;
+          deleteBtn.classList.add("delete");
+          deleteBtn.innerText = "x";
+          deleteBtn.onclick = calendarClickEventHandler.modalDltSchedule;
+  
+          scheduleList.append(date);
+          scheduleList.append(schedule);
+          scheduleList.append(deleteBtn);
+  
+          modalSchedule.append(scheduleList);
+        });
       }
     }
-    else modalSchedule.value = {};
+    else calendar.scheduler.setSchedule({});
 
     calendarModal.style.display = "block";
   },
-  changeMonth : e => {
+  changeMonth: e => {
     let year = e.target.value.split("-")[0],
         month = e.target.value.split("-")[1];
-    calendar.setCalendar(year,month);
+    calendar.setCalendar(year, month);
+    calendar.scheduler.setScheduler(year,month);
     // ajax
     calendar.setSchedule(dbJSON[`d_${calendar.year}_${calendar.month}`]);
     calendarPrint(calendar);
   },
-  modalAddSchedule : e => {
+  modalAddSchedule: () => {
     let scheduleList = document.createElement("li"),
         time = document.createElement("b"),
         schedule = document.createElement("span"),
@@ -175,12 +220,13 @@ const calendarClickEventHandler = {
 
     let hour = Number(modalForm.addTime.value.split(":")[0]),
         minute = Number(modalForm.addTime.value.split(":")[1]);
-    let timeKey = hour*60+minute;
+    let dateSchedule = calendar.scheduler.schedule,
+        timeKey = hour*60+minute;
 
-    if(modalSchedule.value[timeKey]===undefined) {
-      modalSchedule.value[timeKey] = [];
+    if (dateSchedule[timeKey]===undefined) {
+      dateSchedule[timeKey] = [];
     }
-    modalSchedule.value[timeKey].push(modalForm.addSchedule.value);
+    dateSchedule[timeKey].push(modalForm.addSchedule.value);
 
     time.classList.add("time");
     time.innerText = modalForm.addTime.value;
@@ -188,9 +234,9 @@ const calendarClickEventHandler = {
     schedule.innerText = modalForm.addSchedule.value;
 
     deleteBtn.type = "button";
-    deleteBtn.value = `{"time":${timeKey},"index":${modalSchedule.value[timeKey].length-1}}`;
+    deleteBtn.value = `{"time":${timeKey},"index":${dateSchedule[timeKey].length-1}}`;
     deleteBtn.classList.add("delete");
-    deleteBtn.innerText="x";
+    deleteBtn.innerText = "x";
     deleteBtn.onclick = calendarClickEventHandler.modalDltSchedule;
 
     scheduleList.append(time);
@@ -198,9 +244,9 @@ const calendarClickEventHandler = {
     scheduleList.append(deleteBtn);
 
     let timeArr = Array.from(modalForm.getElementsByClassName("time"));
-    if(timeArr.length && time.innerText<timeArr[timeArr.length-1].innerText) {
-      for(let el of timeArr) {
-        if(time.innerText<el.innerText) {
+    if (timeArr.length&&time.innerText < timeArr[timeArr.length-1].innerText) {
+      for (let el of timeArr) {
+        if (time.innerText<el.innerText) {
           el.parentElement.before(scheduleList);
           break;
         }
@@ -212,35 +258,53 @@ const calendarClickEventHandler = {
     modalForm.addTime.value = "00:00";
     modalForm.addSchedule.value = "";
   },
-  modalDltSchedule : e => {
-    let timeKey = e.target.value;
+  modalDltSchedule: e => {
+    let key = JSON.parse(e.target.value),
+        el = e.target.parentElement,
+        schedule = calendar.scheduler.schedule;
+    
+    schedule[key.time].splice(key.index, 1);
+    if (!schedule[key.time].length) {
+      delete schedule[key.time];
+    }
 
-    delete modalSchedule.value[timeKey];
-    e.target.parentElement.remove();
+    let nextEl = el.nextElementSibling;
+    while(nextEl!==null) {
+      let nextBtn = nextEl.lastElementChild;
+      let nextKey = JSON.parse(nextBtn.value);
+
+      if(key.time!=nextKey.time) break;
+      
+      nextKey.index--;
+      nextBtn.value = JSON.stringify(nextKey);
+      
+      nextEl = nextEl.nextElementSibling;
+    }
+
+    el.remove();
   },
-  modalSubmit : e => {
-    let dateSchedule = calendarModal.value.getElementsByClassName("schedule")[0],
-        dateKey = modalDate.value;
+  modalSubmit: () => {
+    let scheduler = calendar.scheduler;
+    let schedule_ul = scheduler.target.getElementsByClassName("schedule")[0];
 
-    Array.from(dateSchedule.children).forEach(item => {
+    Array.from(schedule_ul.children).forEach(item => {
       item.remove();
     });
 
-    if(modalSchedule.value!==undefined){
-      for(let key in modalSchedule.value) {
-        let schedule = document.createElement("li");
+    if (scheduler.schedule!==undefined) {
+      for (let time in scheduler.schedule) {
+        scheduler.schedule[time].forEach(todo => {
+          let scheduleList = document.createElement("li");
 
-        schedule.value = key;
-        schedule.innerText = modalSchedule.value[key];
+          scheduleList.innerText = todo;
 
-        dateSchedule.append(schedule);
+          schedule_ul.append(scheduleList);
+        });
       }
 
-      if(Object.keys(modalSchedule.value).length) {
-        calendar.schedule[dateKey] = modalSchedule.value;
-      }
-      else {
-        delete calendar.schedule[dateKey];
+      calendar.schedule[scheduler.date] = scheduler.schedule;
+      if (!Object.keys(calendar.schedule[scheduler.date]).length) {
+        delete calendar.schedule[scheduler.date];
       }
     }
 
@@ -249,22 +313,20 @@ const calendarClickEventHandler = {
 
     calendarClickEventHandler.modalClose();
   },
-  modalClose : () => {
+  modalClose: () => {
     modalDate.innerText = "";
     Array.from(modalSchedule.children).forEach(el => {
       el.remove();
     });
 
-    modalDate.value = undefined;
-    modalSchedule.value = undefined;
-    calendarModal.value = undefined;
+    calendar.scheduler.setScheduler(calendar.year,calendar.month);
 
     calendarModal.style.display = "none";
   }
 }
 // execute
-const calendar = new Calendar();
-  // ajax
+const calendar = new Calendar(new Date().getFullYear(), new Date().getMonth()+1);
+// ajax
 calendar.setSchedule(dbJSON[`d_${calendar.year}_${calendar.month}`]);
 calendarPrint(calendar);
 // event
