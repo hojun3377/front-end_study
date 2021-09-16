064cd5e02ad8b5a38fff210d462421768eca93a7
hojun3377 <hojun3377@gmail.com>
2021-09-16 17:56:23 +0900

    Study CSS background

---
+++ CSS/script/l01_css_background.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// selector
+const testBox = document.getElementById("testBox");
+const bgTestList = document.getElementById("bgTestList").children;
+
+Array.from(bgTestList).forEach((el, i) => {
+  el.value = {f
+  el.onclick = () => {
+    if(el.value) testBox
+    else
+  }
+});
