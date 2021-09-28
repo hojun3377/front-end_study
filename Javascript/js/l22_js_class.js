764bc2a475fb3ab532fdb06431f6e0a25a0dae36
hojun3377 <hojun3377@gmail.com>
2021-09-28 19:40:04 +0900

    Study JS class_3

--- Javascript/js/l22_js_class.js
+++ Javascript/js/l22_js_class.js
@@ -1,51 +1,57 @@
-// 실습 ( function을 이용하여 생성자 만들기 )
-console.log('실습 ( function을 이용한 생성자 )');
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+console.group("function을 이용한 생성자");
 
 function Car(carName) {
   let name = "qm3";
   this.carName = carName;
-  this.company = this.__proto__.company;
 }
 Car.prototype.company = "hyendae";
 
-console.log("<생성자 함수> ( Car )");
-console.log(Car);
+/////////////////////////////////////////////////////////////////////////////////////////
+console.group("생성자와 프로토타입");
 
-console.log("<생성자 프로토타입> ( Car.prototype )");
-console.log(Car.prototype);
+console.log("생성자 함수 ( Car )\n", Car);
 
-console.log("<새로 생성된 객체> ( 생성자 함수를 이용하여 새로운 객체 생성 )"); // 복제
+console.log("함수의 프로토타입 객체 ( Car.prototype )\n", Car.prototype);
+
 let testCar = new Car("qm3");
-console.log(testCar);
-console.log("<새로 생성된 객체의 __proto__> ( 객체의 __proto__는 생성자의 prototype을 가리킨다. )")
-console.log(testCar.__proto__);
+console.log("인스턴스 객체 ( testCar = new Car('qm3') )\n", testCar);
 
-console.log("<상속>\n<새로운 생성자 함수> ( Wheel )")
+console.log("인스턴스 객체의 __proto__는 생성자의 프로토타입 가리킴 ( testCar.__proto__ )\n", testCar.__proto__);
+
+console.groupEnd();
+/////////////////////////////////////////////////////////////////////////////////////////
+console.group("상속");
+
 function Wheel(carName) {
   Car.call(this,carName); // super 생성자 호출
 }
-console.log(Wheel);
-console.log("<생성자 프로토타입을 복사하여 새로운 프로토타입 객체 생성> ( Object.create(constructorFunction.prototype) )");
-// Car를 상속받는 Wheel로 바인딩
-Wheel.prototype = Object.create(Car.prototype);
+
+console.log("생성자 함수 ( Wheel )\n", Wheel);
+
+console.log("함수의 프로토타입 객체 ( Wheel.prototype )\n", Wheel.prototype);
+
+Wheel.prototype = Object.create(Car.prototype); // Car의 프로토타입을 상속
 Wheel.prototype.constructor = Wheel;
-console.log(Wheel.prototype);
+console.log("생성자 프로토타입을 복사하여 새로운 프로토타입 객체 생성 ( Wheel.prototype = Object.create(prototype) )\n생성자 프로토타입 constructor를 Wheel 함수와 바인딩 ( Wheel.prototype.constructor = Wheel )\n생성자 함수의 프로토타입( Wheel.prototype )\n", Wheel.prototype);
 
-console.log("<Car를 상속받은 Wheel의 새로 생성된 객체>");
-let testCar2 = new Wheel("sonata");
-console.log(testCar2);
-console.log("<새로 생성된 객체의 __proto__>");
-console.log(testCar2.__proto__);
+let testWheel = new Wheel("sonata");
+console.log("인스턴스 객체 ( testWheel = new Wheel('sonata') )\n", testWheel);
 
-//////////////////////////////////////////////////////////////////////
-console.log("//////////////////////////////////////////////////////////////////////");
+console.log("프로토타입 체인 ( testWheel.company )\n", testWheel.company);
 
-// 실습 ( bind함수 )
-console.log('실습 ( bind함수 )');
+console.groupEnd();
+/////////////////////////////////////////////////////////////////////////////////////////
 
-// javascript의 모든 객체는 자신의 prototype으로부터 constructor를 상속한다.
+console.groupEnd();
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+console.group("함수 객체 ( function object )");
+
 // 객체 인스턴스가 생성되는 순간 prototype의 constructor 메서드를 호출하며,
-// 모든 javascript 함수는 function prototype 객체를 참조(__proto__)하기 때문에 function prototype객체의 constructor를 호출하여 생성한다.
+// 모든 javascript 함수는 function prototype 객체를 참조하기 때문에
+// function prototype객체의 constructor를 호출하여 생성한다.
 
 // Person을 선언한 순간 Person.prototype 객체가 생성되며,
 // Person.prototype.constructor는 아래의 원본 함수를 가리킨다.
@@ -55,114 +61,107 @@ function Person() {
     return "안녕, "+this.name;
   }
 }
-console.log("<생성자 함수> ( Person )");
-console.log(Person); // 선언된 함수 ( window객체의 멤버함수 )
-console.log("<객체 내 함수 실행> ( (new Person).callName(); )");
+console.log("생성자 함수 ( Person )\n", Person);
+
 // new Person은 Person.prototype.constructor을 실행하여 새로운 Object를 만들고,
 // 해당 Object의 __proto__는 Person.prototype을 가리킨다.
-console.log((new Person).callName());
+const testPerson = new Person();
+console.log("인스턴스 객체 멤버함수 실행 ( testPerson.callName() )\n", testPerson.callName());
 
-console.log("<생성자 함수 내에 함수 설정> ( Person.callName2 )");
 // Person 함수의 property로 callName2 함수를 선언한다.
+// Person 함수 객체 property로 추가하는 것이므로 실제 선언된 함수가 변경되지 않으며,
+// Person.prototype.constructor를 확인하면 프로퍼티로 추가됨을 알 수 있다. (함수의 메타데이터)
 Person.callName2 = function() {
   // Person.callName2()를 실행시 Person.prototype.constructor.callName2() 가 실행되므로
   // 이때, this는 Person.prototype.constructor 이다.
   // 따라서 Person.prototype.constructor의 name인 'Person'이 출력
   return "안녕, "+this.name;
 };
-// callName2는 이미 선언된 Person 함수 객체 프로퍼티로 추가하는 것이므로 실제 선언된 함수가 변경되지 않으며,
-// Person.prototype.constructor의 프로퍼티로 추가됨을 알 수 있다.
-console.log(Person);
-console.log(Person.prototype);
-console.log("<외부에서 설정한 함수 실행> ( Person.callName2(); )")
-console.log(Person.callName2());
+console.log("생성자 함수 내에 함수 설정 후 호출 ( Person.callName2() )\n", Person.callName2());
+console.log("변하지 않는 생성자 함수 ( Person )\n", Person);
+console.log("Person 함수 객체의 property로 들어간 callName2 ( constructor.callName2() 확인 )\n",Person.prototype);
 
-console.log("<외부에서 설정한 함수 bind 후 실행> ( Person.callName2.bind(new Person()); )")
-const testPerson = new Person();
-// 실행컨텍스트의 객체 testPerson의 프로퍼티로 함수가 바인딩 된다.
-const callName2F = Person.callName2.bind(testPerson);
-console.log(testPerson);
-console.log(callName2F);
-console.log(callName2F());
+console.groupEnd();
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+console.group("bind 함수");
 
-///////////////////////////////////////////////////////////////////////
-console.log("//////////////////////////////////////////////////////////////////////");
+// 함수가 객체 testPerson을 바인딩 한다.
+console.log("callName2를 인스턴스 객체 testPerson에 bind 후 실행 ( Person.callName2.bind(testPerson)() )\n", Person.callName2.bind(testPerson)());
 
-// function과 closure로 이용하여 변수를 캡슐화 하여 정의
-console.log("function과 closure로 이용하여 변수를 캡슐화 하여 정의");
+// 하지만 객체 testPerson의 프로퍼티로 추가되지는 않는다.
+console.log("testPerson의 property로 추가되지 않은 callName2 ( testPerson의 property 확인 )\n", testPerson);
+
+console.groupEnd();
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+console.group("캡슐화");
+
+/////////////////////////////////////////////////////////////////////////////////////////
+console.group("function과 closure");
 
 function TestFunction(name="이름없음",age=0) {
   let _name = name;
   let _age = age;
 
   return {
-    getName: () => {
-      return _name;
-    },
-    getAge: () => {
-      return _age;
-    },
-    setName: (name) => {
-      _name = name;
-    },
-    setAge: (age) => {
-      _age = age;
-    }
+    getName: () => { return _name; },
+    getAge: () => { return _age; },
+    setName: (name) => { _name = name; },
+    setAge: (age) => { _age = age; }
   }
 }
 
-// class를 이용하여 캡슐화 하여 정의
-class TestPerson {
-  // 클래스에서 생략 가능
+const tf = new TestFunction("tf_최호준", 25);
+tf.setName("최호준")
+console.log("function과 closure를 이용한 변수 은닉\n", tf);
+console.log("tf의 변수 접근( tf.getName(), tf.getAge() )\n",`name : ${tf.getName()}, age : ${tf.getAge()}`);
+
+console.groupEnd();
+/////////////////////////////////////////////////////////////////////////////////////////
+console.group("class");
+
+class TestClass {
+  // 일반 변수는 사용하기위해 선언할 때 생략이 가능하지만, private 변수를 사용하기위해 선언할 때 생략이 불가능하다.
+
+  // 생략 가능
   // name = "이름없음"; age = 0;
 
-  // private을 선언하기위해 예약어 # (ex. #name = value;)을 이용하여 변수 선언
   // 생략 불가능
+  // private을 선언하기위해 예약어 # (ex. #name = value;)을 이용하여 변수 선언
   #name;
-  #age
+  #age;
 
-  // 모든 클래스는 생성자를 호출해야 객체를 생성할 수 있다.
+  // 모든 class는 생성자가 존재
   constructor(name="이름없음",age = 0) {
-    // this.name = name ? name : "이름없음";
     this.#name = name;
 
-    try {
-      if(isNaN(age)) {
-        throw `${age} is not a Number`;
-      }
-    }
-    catch(e) {
-      console.log(e)
-    }
-    finally {
-      this.#age = age>=0 ? age : "나이불명";
-    }
+    try { if(isNaN(age)) throw `${age} is not a Number`; }
+    catch(e) { console.log(e); }
+    finally { this.#age = age>=0 ? age : "나이불명"; }
   }
 
-  // getName() { return this.#name+" 씨"; }
-  get name() { return this.#name+" 씨"; }
+  // getter와 setter
+  get name() { return this.#name; }
+  get age() { return this.#age; }
   set name(name) { this.#name = name; }
+  set name(age) { this.#age = age; }
+
+  // 멤버함수
+  hello() { return this.#name+"씨 안녕하세요!"; }
 }
 
-// 1. function과 closure를 이용한 변수은닉
-const tf1 = new TestFunction();
-const tf2 = new TestFunction("tf_최호준", 25);
-console.log(tf1.getName());
-tf2.setName("메롱")
-console.log(tf2.getName());
+const tc = new TestClass("tc_최호준", 25);
+tc.name = "최호준";
+console.log("class를 이용한 변수 은닉\n", tc);
+console.log("tf의 변수 접근( tc.name, tc.age )\n",`name : ${tc.name}, age : ${tc.age}`);
 
-// 2. class를 이용한 변수은닉
-const tp1 = new TestPerson();
-const tp2 = new TestPerson("tp_최호준", 25);
-console.log(tp1.name);
-tp2.name = "가나다"
-console.log(tp2.name);
+console.log("class내에 정의된 함수는 class prototype에 정의됨 ( TestClass.prototype.hello )\n", TestClass.prototype.hello);
 
-///////////////////////////////////////////////////////////////////////
-console.log("//////////////////////////////////////////////////////////////////////");
-
-// 실습 ( class 상속 )
-console.log("실습 ( class 상속 )");
+console.groupEnd();
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+console.group("class 상속");
 
 class Animal {
   #name;
@@ -175,15 +174,18 @@ class Animal {
     this.#species = species;
   }
 
+  // getter
   get name() { return this.#name; }
   get age() { return this.#age; }
   get species() { return this.#species; }
 
+  // setter
   set name(name) { this.#name = name; }
   set age(age) { this.#age = age; }
   set species(species) { this.#species = species; }
-}
 
+  hello() { return this.#name+", 안녕!"; }
+}
 // 주인, 품종, 크기
 class Dog extends Animal {
   // 전역변수 이름과 get 멤버의 이름을 동일하게 작성하지 않기( get 멤버()가 무시당한다.)
@@ -208,5 +210,14 @@ class Dog extends Animal {
   set size(size) { this.#size = size; }
 }
 
+console.log("상위 Class ( Animal )\n", Animal);
+console.log("하위 Class ( Dog )\n", Dog);
+
 const dog = new Dog("코코", 9, "개", "최호준", "푸들", "소형");
-console.log(dog);
+console.log("Animal을 상속받은 Dog의 인스턴스 객체 ( dog = new Dog(...) )\n", dog);
+
+console.log("상위 클래스의 멤버함수 사용 ( dog.hello() )\n", dog.hello());
+
+console.groupEnd();
+/////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
