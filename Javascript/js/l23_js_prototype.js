18315db65355e3d5aa701a5daa320e4a5a31f04f
hojun3377 <hojun3377@gmail.com>
2021-09-30 18:52:32 +0900

    Study JS prototype

--- Javascript/js/l23_js_prototype.js
+++ Javascript/js/l23_js_prototype.js
@@ -3,21 +3,30 @@ function Car() {
   this.move = function() {
     console.log("움직임");
   };
+  function aa(){};
 }
-Car.prototype.move2 = function() {
-  console.log(this);
-  console.log(`${this.name}이 움직임`);
-};
+// Car.prototype.move2 = function() {
+//   console.log(this);
+//   console.log(`${this.name}이 움직임`);
+// };
+Object.defineProperty(Car.prototype, 'move2', {
+  configurable: true,
+  value: function() {
+    console.log(this);
+    console.log(`${this.name}이 움직임`);
+  },
+  writable: true
+});
 
 ////////////////////////////////////////////
 
-console.group("<function Car를 통한 접근>");
+console.group("function Car를 통한 접근");
 console.log("Car", Car);
 console.log("Car.prototype", Car.prototype);
 console.log("Car.name", Car.name);
 console.groupEnd();
 
-console.group("<Car 타입의 객체 car를 통한 접근>");
+console.group("Car 타입의 객체 car를 통한 접근");
 const car = new Car();
 car.move();
 console.log("car", car);
@@ -29,7 +38,7 @@ console.groupEnd();
 
 ////////////////////////////////////////////
 
-console.group("<Object.create(Car)>");
+console.group("Object.create(Car)");
 // car2는 Car를 복제한 독자적인 프로토타입 객체
 // car2.__proto__는 Car를 참조한다.
 const car2 = Object.create(Car);
@@ -59,4 +68,27 @@ console.log("car3.__proto__.__proto__.__proto__", car3.__proto__.__proto__.__pro
 console.groupEnd();
 
 // 익명함수는 프로토타입이 존재하지 않는 함수
-console.log((()=>{}).prototype);
+console.log("익명함수는 프로토타입이 존재하지 않는다. ( (()=>{}).prototype )\n", (()=>{}).prototype);
+
+////////////////////////////////////////////
+
+class Test {
+  constructor(a,b) {
+    this.a = a;
+    this.b = b;
+  };
+  c() { return "c 함수 입니다."; };
+}
+
+const test = new Test("a","b");
+
+console.group("class Test를 이용한 접근");
+console.log("class Test{}", Test);
+console.log("class Test{}.prototype", Test.prototype);
+console.log("class Test{}.__proto__", Test.__proto__);
+console.log("class Test{}.__proto__.__proto__", Test.__proto__.__proto__);
+console.log("class Test{}.prototype.c()", Test.prototype.c());
+console.log("class Test{}.prototype.c.prototype", Test.prototype.c.prototype);
+console.log("const test = new Test('a', 'b')\n", test);
+console.log("test.c.prototype", test.c.prototype);
+console.groupEnd();
